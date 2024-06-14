4fe2e3ba4dfb9d6990f020f862e019cc079f4114
emolokov <e92molokov@gmail.com>
2024-06-14 20:22:46 +0300

    14.06.24 20.22

--- grid.js
+++ grid.js
@@ -13,4 +13,10 @@ export class Grid {
             )
         }
     }
+
+    getRandomEmptyCell() {
+        const emptyCells = this.cells.filter(cell => cell.isEmpty());
+        const randomIndex = Math.floor(Math.random() * emptyCells.length);
+        return emptyCells[randomIndex];
+    }
 }
